5db769d9c84e83f1e26f2fef1b586fdee29a18bf
itggot-erik-dahlgren <38528646+itggot-erik-dahlgren@users.noreply.github.com>
2019-03-17 22:42:06 +0100

    2019-03-17

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
-require 'byebug'
 require 'bcrypt'
 
 enable :sessions
@@ -18,6 +17,25 @@ get ('/create') do
     slim(:create)
 end
 
+get ('/error') do
+    slim(:error)
+end
+
+get ('/:user_id/profile') do
+    if session[:Username].nil?
+        slim(:error)
+    else
+    db = SQLite3::Database.new("db/users.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT Username, Information, Post_Id, User_Id FROM posts")
+    slim(:profile, locals: {
+        user: session[:Username],
+        posts: result
+    })
+    end
+end
+
 post ('/create/new') do
     db = SQLite3::Database.new("db/users.db")
     db.results_as_hash = true
@@ -29,28 +47,52 @@ post ('/create/new') do
     redirect('/')
 end
 
+get ("/profile/:post_id/edit_post") do
+    slim(:edit_post, locals: {
+        user: session[:Post_Id]
+    })
+end
+
 post ('/inlogg/verify') do
     db = SQLite3::Database.new("db/users.db")
     db.results_as_hash = true
 
-    result = db.execute("SELECT Username, Password FROM users WHERE Username = ?", params["name"])
+    result = db.execute("SELECT User_Id, Username, Password FROM users WHERE Username = ?", params["name"])
     if result.length > 0 && BCrypt::Password.new(result.first["Password"]) == params["password"]
         session[:Username] = result.first["Username"]
-        redirect("/profile") ## redirect("/profile/:Username") Försök att supportera fler än 1 user
+        session[:User_Id] = result.first["User_Id"]
+        redirect("/#{session[:User_Id]}/profile")
     else
-        redirect('/')
+        redirect('/error')
     end
 end
 
-post ('/post') do
+post ('/:user_id/post') do
     db = SQLite3::Database.new("db/users.db")
     db.results_as_hash = true
 
     post = params["textarea"]
-    result = db.execute("INSERT INTO posts (Username, Information) VALUES (?,?)", session[:Username], post)
-    redirect('/profile')
+    if post.length > 0
+        result = db.execute("INSERT INTO posts (Username, Information, User_Id) VALUES (?,?,?)", session[:Username], post, session[:User_Id])
+        redirect("/#{session[:User_Id]}/profile")
+    else
+        redirect('/error')
+    end
 end
 
-get ('/profile') do
-    slim(:profile, locals: {user: session[:Username]})
+post ('/edit_post') do
+    db = SQLite3::Database.new('db/users.db')
+    db.results_as_hash = true
+
+    post = params["textarea"]
+    db.execute("UPDATE posts SET Information='#{post}' WHERE Post_Id=?", session[:Post_Id])
+    redirect("/#{session[:User_Id]}/profile")
+end
+
+post ('/profile/:post_id/delete_post') do
+    db = SQLite3::Database.new('db/users.db')
+    db.results_as_hash = true
+
+    db.execute("DELETE FROM posts WHERE Post_Id=?", session[:Post_Id])
+    redirect("#{session[:User_Id]}/profile")
 end
